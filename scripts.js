a5febd67d89c936aef2cfddd1653f3f16bc06fd8
JuampiLeoz <mdqalumno@gmail.com>
2024-05-25 16:48:00 -0300

    se trajo todo a main

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function validarCor (mail){
+    let expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    let corValid= expReg.test(mail);
+
+if(corValid==true){
+    alert 
+}
+}
