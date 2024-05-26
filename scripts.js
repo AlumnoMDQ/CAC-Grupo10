e672fb44ddd1af4885c16fdbb087bc9cb67e259d
JuampiLeoz <mdqalumno@gmail.com>
2024-05-26 17:35:28 -0300

    validacion con javascript mejorada 2.0

--- scripts.js
+++ scripts.js
@@ -9,18 +9,35 @@ const inputs = [
 const nombreInput = /** @type {HTMLInputElement} */ (
     document.querySelector("input[name='nombre']")
 );
+const anosInput = /** @type {HTMLInputElement} */ (
+    document.querySelector("input[name='años']")
+);
 
 const containsNumbers = (str) => /\d/.test(str);
+const containsLetters = (str) => /[a-zA-Z]/.test(str);
 
 const invalidForm = () => {
-    return inputs.some(input => !input.validity.valid) || containsNumbers(nombreInput.value);
-    alert("No se admiten numeros en nombre")
+    return inputs.some(input => !input.validity.valid) || containsNumbers(nombreInput.value) || containsLetters(anosInput.value);
+};
+
+const checkNombreInput = () => {
+    if (containsNumbers(nombreInput.value)) {
+        alert("El campo 'nombre' no puede contener números.");
+    }
+};
+
+const checkAnosInput = () => {
+    if (containsLetters(anosInput.value)) {
+        alert("El campo 'años' no puede contener letras.");
+    }
 };
 
 inputs.forEach(input =>
-    input.addEventListener("input", () => 
-        Object.assign(submitButton, { disabled: invalidForm() }),
-    )
+    input.addEventListener("input", () => {
+        checkNombreInput();
+        checkAnosInput();
+        Object.assign(submitButton, { disabled: invalidForm() });
+    })
 );
 
 Object.assign(submitButton, { disabled: invalidForm() });
