68d4979ef80bca5bc35977a4f3ea68bbbeef6fd3
JuampiLeoz <mdqalumno@gmail.com>
2024-05-26 20:30:30 -0300

    fix del validador

--- scripts.js
+++ scripts.js
@@ -6,38 +6,10 @@ const inputs = [
         document.querySelectorAll("form input, form textarea, form select")
     ),
 ];
-const nombreInput = /** @type {HTMLInputElement} */ (
-    document.querySelector("input[name='nombre']")
-);
-const anosInput = /** @type {HTMLInputElement} */ (
-    document.querySelector("input[name='años']")
-);
-
-const containsNumbers = (str) => /\d/.test(str);
-const containsLetters = (str) => /[a-zA-Z]/.test(str);
-
-const invalidForm = () => {
-    return inputs.some(input => !input.validity.valid) || containsNumbers(nombreInput.value) || containsLetters(anosInput.value);
-};
-
-const checkNombreInput = () => {
-    if (containsNumbers(nombreInput.value)) {
-        alert("El campo 'nombre' no puede contener números.");
-    }
-};
-
-const checkAnosInput = () => {
-    if (containsLetters(anosInput.value)) {
-        alert("El campo 'años' no puede contener letras.");
-    }
-};
+const invalidForm = () => inputs.some(input => !input.validity.valid);
 
 inputs.forEach(input =>
-    input.addEventListener("input", () => {
-        checkNombreInput();
-        checkAnosInput();
-        Object.assign(submitButton, { disabled: invalidForm() });
-    })
-);
-
-Object.assign(submitButton, { disabled: invalidForm() });
+    input.addEventListener("input", () =>
+        Object.assign(submitButton, { disabled: invalidForm() }),
+    ),
+);      
