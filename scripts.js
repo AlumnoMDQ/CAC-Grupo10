d31a55eb3fa620c476907535e1c33339a89f13b1
JuampiLeoz <mdqalumno@gmail.com>
2024-05-25 22:05:11 -0300

    intentando no romper nada

--- scripts.js
+++ scripts.js
@@ -1,8 +1,4 @@
-function validarCor (mail){
-    let expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-    let corValid= expReg.test(mail);
-
-if(corValid==true){
-    alert 
-}
-}
+document.querySelector("form").addEventListener("submit", event=>{
+    event.preventDefault();
+    console.log(event.currentTarget);
+})
