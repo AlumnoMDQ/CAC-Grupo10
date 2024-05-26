85ba54a9f33ced157caa24fc223c991c6c5a4252
JuampiLeoz <mdqalumno@gmail.com>
2024-05-25 22:25:14 -0300

    validacion completada

--- scripts.js
+++ scripts.js
@@ -1,4 +1,22 @@
-document.querySelector("form").addEventListener("submit", event=>{
-    event.preventDefault();
-    console.log(event.currentTarget);
-})
+const submitButton = /** @type {HTMLButtonElement} */ (
+    document.querySelector("button.botonEnviar")
+);
+const inputs = [
+    .../** @type {NodeListOf<HTMLInputElement>} */ (
+        document.querySelectorAll("form input, form textarea, form select")
+    ),
+];
+const invalidForm = () => inputs.some(input => !input.validity.valid);
+
+inputs.forEach(input =>
+    input.addEventListener("input", () =>
+        Object.assign(submitButton, { disabled: invalidForm() }),
+    ),
+);
+
+/*
+
+const formData = Object.fromEntries(new FormData(event.currentTarget));
+
+traer data como objeto
+*/ 
