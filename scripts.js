5b1f50279778b0dd81afada3234d8f5d282daf49
Cata <164280315+cata245@users.noreply.github.com>
2024-06-23 19:29:28 -0300

    Update scripts.js
    Me saltaba que tenía un error para mostrar el mensaje, si el update está mal lo cambio a como estaba antes

--- scripts.js
+++ scripts.js
@@ -1,11 +1,13 @@
 const submitButton = /** @type {HTMLButtonElement} */ (
     document.querySelector("button.botonEnviar")
 );
+
 const inputs = [
     .../** @type {NodeListOf<HTMLInputElement>} */ (
         document.querySelectorAll("form input, form textarea, form select")
     ),
 ];
+
 const nombreInput = /** @type {HTMLInputElement} */ (
     document.querySelector("input[name='nombre']")
 );
@@ -13,12 +15,17 @@ const nombreInput = /** @type {HTMLInputElement} */ (
 const containsNumbers = (str) => /\d/.test(str);
 
 const invalidForm = () => {
-    return inputs.some(input => !input.validity.valid) || containsNumbers(nombreInput.value);
-    alert("No se admiten numeros en nombre")
+    if (containsNumbers(nombreInput.value)) {
+        alert("No se admiten números en el nombre");
+        return true; // Devuelve true indicando que el formulario es inválido
+    }
+
+    // Verificar la validez de los demás campos de entrada
+    return inputs.some(input => !input.validity.valid);
 };
 
 inputs.forEach(input =>
     input.addEventListener("input", () => 
         Object.assign(submitButton, { disabled: invalidForm() }),
     ),
-);      
+);    
